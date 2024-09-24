b3c0c3443d5562a29294f7b09008c1c948c55b57
NikAfz <nikafz980@gmail.com>
2024-09-24 19:38:19 +0330

    fix : home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 // import ReactDOM  from "react-dom";
 import NavBar from "./NavBar";
-import SearchBar from "./Search"
+import SearchBar from "./Search";
 // import SearchInfo from "./SearchInfo";
 import CoinListElements from "./coinListElements";
 import PremiumList from "./SubscribeList";
 import Footer from "./Footer";
 
 function Home(props) {
+  const [inputValue, setInputValue] = useState("");
   return (
     <>
-      <SearchBar />
+      <SearchBar inputValue={inputValue} setInputValue={setInputValue} />
       {/* <SearchInfo /> */}
       <main>
-        <CoinListElements />
+        <CoinListElements inputValue={inputValue} />
         <PremiumList />
       </main>
     </>
-  )
-};
+  );
+}
 
 export default Home;
